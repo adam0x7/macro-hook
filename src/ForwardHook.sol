da344b97e7b7a431a48d762d97b94da03302f558
adam0x7 <18746720+adam0x7@users.noreply.github.com>
2024-05-31 23:59:29 -0400

    adding hooks to test

--- src/ForwardHook.sol
+++ src/ForwardHook.sol
@@ -5,9 +5,9 @@ import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {Currency} from "v4-core/types/Currency.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
-import {BaseHook} from "v4-periphery/contracts/BaseHook.sol";
-
-
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 contract ForwardHook is BaseHook {
 
     constructor(IPoolManager _manager) BaseHook(_manager){}
@@ -43,9 +43,20 @@ contract ForwardHook is BaseHook {
         PoolKey calldata key,
         IPoolManager.SwapParams calldata params,
         bytes calldata
-    ) external override returns (bytes4, BeforeSwapDelta) {
-        return (this.beforeSwap.selector, beforeSwapDelta);
+    ) external override returns (bytes4, BeforeSwapDelta, uint24) {
+        BeforeSwapDelta beforeSwapDelta;
+        return (this.beforeSwap.selector, beforeSwapDelta, 0);
     }
 
+    function afterAddLiquidity(
+        address,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) external virtual override returns (bytes4, BalanceDelta) {
+        BalanceDelta balanceDelta;
+        return (this.afterAddLiquidity.selector, balanceDelta);
+    }
 
 }
